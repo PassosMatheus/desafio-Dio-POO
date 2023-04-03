d3861b6814691c76476de6348746f93b540c33c5
Matheus Passos <matheus.martinspassos@yahoo.com>
2023-04-03 16:30:55 -0300

    Finalizado o código de herança e poliformismo.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
-
+import br.com.dio.desafio.dominio.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
@@ -15,13 +15,46 @@ public class Main {
         curso2.setDescricao("Descrição do curso");
         curso2.setCargaHoraria(8);
 
+        Conteudo conteudo = new Curso();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descricao da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*stem.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao bem legal do bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos Matheus: " + devMatheus.getConteudosInscrito());
+
+        devMatheus.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos Matheus: " + devMatheus.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Matheus: " + devMatheus.getConteudosConcluidos());
+        System.out.println("XP:" + devMatheus.calcularTotalXp());
+
+        System.out.println("---------------------");
+
+        Dev devIsabele = new Dev();
+        devIsabele.setNome("Isabele");
+        devIsabele.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos Isabele: " + devIsabele.getConteudosInscrito());
+
+        devIsabele.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos Isabele: " + devIsabele.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Isabele: " + devIsabele.getConteudosConcluidos());
+        System.out.println("XP:" + devIsabele.calcularTotalXp());
     }
 }
